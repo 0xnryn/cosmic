e0c631052bd6115b8822c5861703cd4019a597ca
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-05-28 17:01:39 +0530

    udpate

--- secrets.nix
+++ secrets.nix
@@ -3,7 +3,6 @@ let
   # nix run github:ryantm/agenix -- --rekey
   sudha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN/N5EHrKFPyXEgKpx5ew6yDnKbPXFkdTmy2hQoV/v8d"; 
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQMz2mXavnOldYheOwKDpX5M9fRMOZ3Ib0N06PexkQh";
-  # lenovoideapadgaming3_tpm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI...";
   allKeys = [ sudha laptop];
 in
 {
