75aa56ece7f8a8cae55208e79e62af2c60e57224
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-05-29 22:58:06 +0530

    update

--- secrets.nix
+++ secrets.nix
@@ -3,7 +3,7 @@ let
   # nix run github:ryantm/agenix -- --rekey
   sudha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN/N5EHrKFPyXEgKpx5ew6yDnKbPXFkdTmy2hQoV/v8d"; 
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQMz2mXavnOldYheOwKDpX5M9fRMOZ3Ib0N06PexkQh";
-  allKeys = [ sudha laptop];
+  allKeys = [ sudha laptop ];
 in
 {
   "secrets/sshsudha.age".publicKeys = allKeys;
