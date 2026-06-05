24e8b46fd55c98ee6a429146d2cee526b9e947dc
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-06-05 15:11:49 +0530

    udpate

--- flake.nix
+++ flake.nix
@@ -15,15 +15,14 @@
     };
   };
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      imports = [
-        (inputs.import-tree ./core)
-      ];
-      flake.flakeModules.default = {
-        imports = inputs.import-tree ./core;
-      };
-    };
+  inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    imports = [
+      (inputs.import-tree ./core)
+    ];
+
+    flake.flakeModules.default = inputs.import-tree ./core;
+  };
 }
 
 
